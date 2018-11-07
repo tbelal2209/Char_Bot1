773163dbd1897e402cc8e1a34a2ae0ef2e9c7f2b
BT_1N3_07@bktech.local <BT_1N3_07@bktech.local>
2018-11-07 08:36:34 -0500

    Initial commit

---
+++ src/com/company/ChatBotRunner.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ChatBotRunner
+{
+
+
+    public static void main(String[] args)
+    {
+        ChatBot1 chatbot1 = new ChatBot1();
+
+
+
+        Scanner in = new Scanner (System.in);
+        System.out.println("Welcome to the chatbot, nice to meet you.");
+        String statement = in.nextLine();
+
+
+        while (!statement.equals("Bye"))
+        {
+            //Use Logic to control which chatbot is handling the conversation\
+            //This example has only chatbot1
+
+
+
+            chatbot1.chatLoop(statement);
+
+
+            statement = in.nextLine();
+
+
+        }
+    }
+
+}
